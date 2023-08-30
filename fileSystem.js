c8b38d845bb123bcc3002289c88846d30d61239a
Maxfuente <max.fuentes.a@gmail.com>
2023-08-29 20:40:12 -0400

    up

--- fileSystem.js
+++ fileSystem.js
@@ -1,59 +1,80 @@
-import {promises as fs} from "fs"
+    import {  promises as fs } from "fs";
 
-class ProductManager   {
-    constructor(){
-        this.patch = "./productos.txt"
-        this.products =[]
+class ProductManager {
+    constructor() {
+        this.patch = "./productos.txt";
+        this.products = [];
     }
 
-    static id = 0
+    static id = 0;
 
-    addProduct = async(titulo,  descripcion,precio, imagen, codigo, stock) => {
+    addProduct = async (titulo, descripcion, precio, imagen, codigo, stock) => {
 
-        ProductManager.id++
+        ProductManager.id++;
 
-        let newProduct  = {
-            titulo,  descripcion,precio, imagen, codigo, stock , id: ProductManager.id
-        }
+        let newProduct = {
+            titulo, descripcion, precio, imagen, codigo, stock, id: ProductManager.id
+        };
 
-        this.products.push(newProduct)
+        this.products.push(newProduct);
 
-        await fs.writeFile(this.patch, JSON.stringify(this.products))
+        await fs.writeFile(this.patch, JSON.stringify(this.products));
+    };
+
+    readProducts = async () => {
+        let respuesta = await fs.readFile(this.patch, "utf-8");
+        return JSON.parse(respuesta);
+    };
+
+    getProducts = async () => {
+        let respuesta2 = await this.readProducts()
+        return console.log(respuesta2)
+
     }
-
-    readProducts = async()=>{
-        let respuesta =  await fs.readFile(this.patch,"utf-8")
-        return JSON.parse(respuesta)
-    }
-
-    getProducts = async()=>{
-      let respuesta2 =  await this.readProducts()
-      return console.log(respuesta2)
-
-    }
-    getProductsById = async(id)=>{
-        let respuesta3 =  await this.readProducts()  
-        if (!respuesta3.find(products => products.id===id)){
+    getProductsById = async (id) => {
+        let respuesta3 = await this.readProducts();
+        if (!respuesta3.find((product) => product.id === id)){
             console.log("No existe");
         } else {
-            console.log(respuesta3.find(products => products.id===id))
+            console.log(respuesta3.find((product) => product.id === id));
         }
 
     }
 
-
-    deleteProductsById = async(id) =>{
+    deleteProductsById = async (id) => {
         let respuesta3 = await this.readProducts();
-        let productFilter = respuesta3.filter(products => products.id != id)
-        await fs.writeFile(this.patch, JSON.stringify(productFilter))
+        let productFilter = respuesta3.filter((products) => products.id != id);
+        await fs.writeFile(this.patch, JSON.stringify(productFilter));
+        console.log("products deleted")
     }
+
+    updateProducts = async ({id, ...producto})=>{
+        await this.deleteProductsById(id);
+        let productOld = await this.readProducts();
+        let productmodif = [{ ...producto, id}, ...productOld ];
+        await fs.writeFile(this.patch,JSON.stringify(productmodif));
     }
+
+}
 const productos = new ProductManager
 
 
-productos.addProduct('producto de prueba','descripcion1','5000', 'imagen1', 'abc123','25')
-productos.addProduct('producto de prueba2','descripcion2','8000', 'imagen2', 'abc456','20')
+/*productos.addProduct('producto de prueba', 'descripcion1', '5000', 'imagen1', 'abc123', '25')
+productos.addProduct('producto de prueba2', 'descripcion2', '8000', 'imagen2', 'abc456', '20')
+productos.addProduct('producto de prueba3', 'descripcion3', '9000', 'imagen3', 'abc789', '30')*/
 
-//productos.getProducts()
+productos.getProducts()
+
 //productos.getProductsById(3)
-productos.deleteProductsById(2)
+
+//productos.deleteProductsById(2)
+
+/*productos.updateProducts({
+    titulo: 'producto de prueba3',
+    descripcion: 'descripcion3',
+    precio: '4000',
+    imagen: 'imagen3',
+    codigo: 'abc789',
+    stock: '30',
+    id: 3
+})*/
